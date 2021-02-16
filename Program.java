ed95169d702a1d2a38677428abbf32403ebe41e2
Alexey <69649787+Achichev@users.noreply.github.com>
2021-02-16 19:23:57 +0300

    Update Program.java

--- Program.java
+++ Program.java
@@ -13,7 +13,7 @@ public class Program  {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.mail.ru");
+        properties.put("mail.smtp.host", "smtp.mail.ru"); //for gmail.com use port 465
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.ssl.trust", "smtp.mail.ru");
 
